b65916665b4657dbc4ac167d5b3f9d93a3db8d14
yutaokkots <yuta.okkotsu@gmail.com>
2023-12-11 11:26:38 -0700

    Project setup and initialization.

---
+++ useraccounts/urls.py
@@ -0,0 +1,7 @@
+"""Module providing URL patterns for useraccounts-related API views"""
+from django.urls import path
+
+# 'api/auth/'
+urlpatterns = [
+    # path("", Class.as_view(), name="getuser"),
+]
